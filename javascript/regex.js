709eef9d4163e49f814acb164b6ba428dca8fdf1
Gabe <G_Linski@hotmail.com>
2024-03-07 23:26:07 -0600

    regex start

---
+++ javascript/regex.js
@@ -0,0 +1,88 @@
+/*
+
+https://www.w3schools.com/js/js_regexp.asp
+https://www.w3schools.com/jsref/jsref_obj_regexp.asp
+
+
+There isn't much point in listing everything.
+That can be done much better in the cheat sheets
+
+
+*/
+
+
+
+/*----------------------------------------------------------------------*/ 
+//Regular Expressions
+/*
+Used to match certain keys/parts of a string
+
+
+
+*/
+
+
+
+/*----------------------------------------------------------------------*/ 
+// Regex Cheat Sheets
+// https://quickref.me/regex.html
+// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Guide/Regular_expressions/Cheatsheet
+// https://www.debuggex.com/cheatsheet/regex/javascript
+// https://code.tutsplus.com/a-simple-regex-cheat-sheet--cms-31278t
+// https://dev.to/ruppysuppy/the-regular-expression-regex-cheat-sheet-you-always-wanted-1c8h
+// https://dev.to/catherinecodes/a-regex-cheatsheet-for-all-those-regex-haters-and-lovers--2cj1
+// https://www.keycdn.com/support/regex-cheat-sheet
+// https://digitalfortress.tech/tips/top-15-commonly-used-regex/
+// https://www.rexegg.com/regex-quickstart.html
+
+
+
+/*----------------------------------------------------------------------*/ 
+//Regex Generators
+//https://regex-generator.olafneumann.org/?sampleText=2020-03-12T13%3A34%3A56.123Z%20INFO%20%20%5Borg.example.Class%5D%3A%20This%20is%20a%20%23simple%20%23logline%20containing%20a%20%27value%27.&flags=i
+// https://regexr.com/
+// https://regex101.com/
+// https://regex.ai/
+// https://retool.com/utilities/regex-generator
+
+
+
+
+/*----------------------------------------------------------------------*/ 
+// Basics
+
+
+/*----------------------------------------------------------------------*/ 
+//Common Regexes
+
+
+
+/*----------------------------------------------------------------------*/ 
+
+
+/*----------------------------------------------------------------------*/ 
+
+
+
+/*----------------------------------------------------------------------*/ 
+
+
+
+/*----------------------------------------------------------------------*/ 
+
+
+
+/*----------------------------------------------------------------------*/ 
+
+
+/*----------------------------------------------------------------------*/ 
+
+/*----------------------------------------------------------------------*/ 
+
+
+
+
+
+
+
+
